565fcbd9757e8021e937c6958d28f033d402284d
Sarah Fullmer <sarah.fullmer@gmail.com>
2023-02-16 11:55:49 +0100

    created questions pool

---
+++ lib/questions.dart
@@ -0,0 +1,16 @@
+const questions = [
+  'Who is the oldest?',
+  'Who is the tallest?',
+  'Who is the youngest?',
+  'Who is the shortest?',
+  'Who has the longest hair?',
+  'Who has the shortest hair?',
+  'Who was the last person to travel abroad?',
+  'Who was the last person to buy a game?',
+  'Who has the largest physical game collection?',
+  'Who speaks the most languages?',
+  'Who lost the last game?',
+  'Who can touch their nose with their tongue?',
+  'Who\'s birthday is earliest in the year?',
+  'Who\'s birthday is latest in the year?'
+];
